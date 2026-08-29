a2104a9fff6d3b5a1f8198bdf446f53bdb093fe7
VershininMA <passad24@ya.ru>
2026-08-29 15:49:54 +0300

    4

---
+++ Assets/_Project/Scripts/Core/GridGenerator.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class GridGenerator : MonoBehaviour
+{
+    [Header("Размеры поля")]
+    public int width = 10;
+    public int height = 10;
+
+    [Header("Префаб ячейки")]
+    public GameObject tilePrefab; 
+
+    private void Start()
+    {
+        GenerateGrid();
+    }
+
+    private void GenerateGrid()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int z = 0; z < height; z++)
+            {
+                // Спавним клетки на плоской поверхности XZ
+                Vector3 spawnPos = new Vector3(x, 0f, z);
+                Instantiate(tilePrefab, spawnPos, Quaternion.identity, transform);
+            }
+        }
+    }
+}
